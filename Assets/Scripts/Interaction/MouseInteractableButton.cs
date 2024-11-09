d0a19ea3538646a79a03392697d759d2b14a1741
JohnDoubleUB <JohnDoubleUB@gmail.com>
2024-11-09 14:57:14 +0000

    Proof of concept button and display
    Proof of concept button and display

--- Assets/Scripts/Interaction/MouseInteractableButton.cs
+++ Assets/Scripts/Interaction/MouseInteractableButton.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using UnityEngine.Events;
+using static UnityEngine.UI.Button;
 
 public class MouseInteractableButton : MouseInteractable
 {
     [SerializeField]
     Animator ButtonAnimator;
+
+    [SerializeField]
+    private UnityEvent m_OnInteractDown = new UnityEvent();
+
+    [SerializeField]
+    private UnityEvent m_OnInteractUp = new UnityEvent();
     protected override void OnInteract(bool value)
     {
         if (ButtonAnimator != null) 
         {
             ButtonAnimator.SetBool("Press", value);
+        }
+
+        if (value)
+        {
+            m_OnInteractDown?.Invoke();
+        }
+        else
+        {
+            m_OnInteractUp?.Invoke();
         }
     }
 }
