65408ec695bb41595974b496fd6417bb2a677189
JohnDoubleUB <JohnDoubleUB@gmail.com>
2024-11-12 22:12:23 +0000

    More switch puzzle logic
    More switch puzzle logic

--- Assets/Scripts/Puzzles/SwitchPuzzle.cs
+++ Assets/Scripts/Puzzles/SwitchPuzzle.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [System.Serializable]
@@ -15,16 +17,53 @@ public class SwitchPuzzle : Puzzle
     [SerializeField]
     private SwitchAndLight[] SwitchAndLights;
 
+    [SerializeField]
+    private IndicatorLight[] ShuffledLights;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Determine if it is 2 or 3 
+        int enabledSwitchCount = 2;//Random.value > 0.7f ? 2 : 3;
+
+        int linkedSwitchCount = 3;
+
+        int[] onSwitches = new int[SwitchAndLights.Length];
+
+        for (int i = 0; i < enabledSwitchCount;) 
+        {
+            int randomIndex = Random.Range(0, onSwitches.Length);
+
+            if (onSwitches[randomIndex] < 1) 
+            {
+
+                onSwitches[randomIndex] = 1;
+                if(randomIndex+1 < onSwitches.Length) 
+                {
+                    onSwitches[randomIndex+1] = 2;
+                }
+
+                if (randomIndex - 1 > 0) 
+                {
+                    onSwitches[randomIndex - 1] = 2;
+                }
+                i++;
+            }
+        }
+
+        ShuffledLights = SwitchAndLights.Select(x => x.Light).ToArray();
+        ShuffledLights.Shuffle();
+
         for (int i = 0; i < SwitchAndLights.Length; i++) 
         {
             SwitchAndLight switchAndLight = SwitchAndLights[i];
             switchAndLight.Switch.Index = i;
             switchAndLight.Switch.PuzzleBrain = this;
 
-            switchAndLight.Switch.SetSwitchValue(false, Random.value > 0.5f);
+            bool shouldStartOn = onSwitches[i] == 1;
+
+            switchAndLight.Switch.SetSwitchValue(shouldStartOn, Random.value > 0.5f);
+            switchAndLight.Light.SetLight(shouldStartOn ? LightColor.Green : LightColor.None, 0);
         }
     }
 
@@ -37,6 +76,12 @@ public class SwitchPuzzle : Puzzle
     public override void SwitchValue(bool value, int index)
     {
         SwitchAndLight switchAndLight = SwitchAndLights[index];
-        switchAndLight.Light.SetLight(value ? LightColor.Green : LightColor.None, 0);
+        switchAndLight.Light.SetLight(!switchAndLight.Light.isOn ? LightColor.Green : LightColor.None, 0);
+        
+        IndicatorLight shuffledLight = ShuffledLights[index];
+        if (shuffledLight != switchAndLight.Light) 
+        {
+            shuffledLight.SetLight(!shuffledLight.isOn ? LightColor.Green : LightColor.None, 0);
+        }
     }
 }
