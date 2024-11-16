a34bf09572139313c7d3276d0836dc2e33780345
JohnDoubleUB <JohnDoubleUB@gmail.com>
2024-11-16 12:23:41 +0000

    WIP Wire Setup

---
+++ Assets/Scripts/PositionMover.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PositionMover : MonoBehaviour
+{
+    public Transform TargetTransform;
+
+    private void FixedUpdate()
+    {
+        if(TargetTransform == null) 
+        {
+            return;
+        }
+
+        transform.position = TargetTransform.position;
+    }
+}
